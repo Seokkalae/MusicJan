9c820c9e5c99192dc6fe025a4dfda62b569882cb
seokkalae <unknown@>
2024-03-06 00:54:49 +0300

    init

---
+++ src/main/java/org/seokkalae/musicjan/MusicjanApplication.java
@@ -0,0 +1,13 @@
+package org.seokkalae.musicjan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicjanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicjanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/seokkalae/musicjan/MusicjanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seokkalae.musicjan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicjanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
