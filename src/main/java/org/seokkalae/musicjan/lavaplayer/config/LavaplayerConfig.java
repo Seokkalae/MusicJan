b5b51b95fd52860f5bceaea7cd313ec3e4fd464f
seokkalae <unknown@>
2024-03-09 07:24:58 +0300

    can set youtube email and pass for add age restricted track

--- src/main/java/org/seokkalae/musicjan/lavaplayer/config/LavaplayerConfig.java
+++ src/main/java/org/seokkalae/musicjan/lavaplayer/config/LavaplayerConfig.java
@@ -4,6 +4,8 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -11,8 +13,17 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class LavaplayerConfig {
     @Bean
-    public AudioPlayerManager audioPlayerManager() {
+    public AudioPlayerManager audioPlayerManager(
+            @Value("${youtube.email}") String youtubeEmail,
+            @Value("${youtube.password}") String youtubePassword
+    ) {
         var defaultAudioPlayerManager = new DefaultAudioPlayerManager();
+        defaultAudioPlayerManager
+                .registerSourceManager(new YoutubeAudioSourceManager(
+                        true,
+                        youtubeEmail,
+                        youtubePassword)
+                );
         AudioSourceManagers.registerRemoteSources(defaultAudioPlayerManager);
         return defaultAudioPlayerManager;
     }
